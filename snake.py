b68037394c370ddb9cb8bbebee33870910c69810
worldintro <worldintro@mail.ru>
2018-10-21 23:24:22 +0300

    v0.1

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from random import randint
+from random import randrange
 import time
 import keyboard
 
@@ -13,50 +14,53 @@ canvas = Canvas(root, width = WIDTH, height = HEIGHT, bg = '#2f353b')
 canvas.pack()
 lastPressed = 'a'
 snake = [[300,300], [320, 300], [340, 300], [360, 300], [380, 300]]
-
-def drawSnake(snake):
+#draw snake and apple
+def draw(snake, x, y):
     canvas.delete("all")
     for dot in snake:
         canvas.create_rectangle(dot[0], dot[1], dot[0] + SIZE_BLOCK, dot[1]+SIZE_BLOCK, fill = '#dad871', outline = '#dad871')
+    canvas.create_rectangle(x, y, x + SIZE_BLOCK, y+SIZE_BLOCK, fill = '#c51d34', outline = '#c51d34')
 #pop element and update picture
-def popAndDraw(snake):
-    snake.pop()
+def popAndDraw(snake, x, y):
+    if(snake[0]!=[x,y]):
+        snake.pop()
     time.sleep(0.1)
-    drawSnake(snake)
+    draw(snake, x, y)
     root.update()
-
-def move(snake, lastPressed):
+#move and check eatd apple
+def move(snake, lastPressed, x, y):
     if(lastPressed == 'w'):
         snake.insert(0, [snake[0][0], snake[0][1]-SIZE_BLOCK])
-        popAndDraw(snake)
+        popAndDraw(snake, x, y)
     if(lastPressed == 'a'):
         snake.insert(0, [snake[0][0]-SIZE_BLOCK, snake[0][1]])
-        popAndDraw(snake)
+        popAndDraw(snake, x, y)
     if(lastPressed == 'd'):
         snake.insert(0, [snake[0][0]+SIZE_BLOCK, snake[0][1]])
-        popAndDraw(snake)
+        popAndDraw(snake, x, y)
     if(lastPressed == 's'):
         snake.insert(0, [snake[0][0], snake[0][1]+SIZE_BLOCK])
-        popAndDraw(snake)
-
-drawSnake(snake)
+        popAndDraw(snake, x, y)
+#coord of apple
+x = randrange(0, 580, 20)
+y = randrange(0, 580, 20)
+draw(snake, x, y)
 root.update()
 while (keyboard.is_pressed('q') == False):
-    move(snake, lastPressed)
     if(keyboard.is_pressed('w')&(lastPressed != 's')):
-        snake.insert(0, [snake[0][0], snake[0][1]-SIZE_BLOCK])
-        popAndDraw(snake)
         lastPressed = 'w'
     if(keyboard.is_pressed('a')&(lastPressed != 'd')):
-        snake.insert(0, [snake[0][0]-SIZE_BLOCK, snake[0][1]])
-        popAndDraw(snake)
         lastPressed = 'a'
     if(keyboard.is_pressed('d')&(lastPressed != 'a')):
-        snake.insert(0, [snake[0][0]+SIZE_BLOCK, snake[0][1]])
-        popAndDraw(snake)
         lastPressed = 'd'
     if(keyboard.is_pressed('s')&(lastPressed != 'w')):
-        snake.insert(0, [snake[0][0], snake[0][1]+SIZE_BLOCK])
-        popAndDraw(snake)
         lastPressed = 's'
+    move(snake, lastPressed, x, y)
+    if(snake[0]==[x,y]):
+        x = randrange(0, 580, 20)
+        y = randrange(0, 580, 20)
+
 root.mainloop()
+
+def insertDot(snake, ud, rl):
+    snake.insert(0, [snake[0][0]+ud, snake[0][1]+rl])
