ed94a4bd61f9da26d177fc6539700f86f2c4a879
worldintro <worldintro@mail.ru>
2018-10-23 23:29:30 +0300

    v0.4

--- snake.py
+++ snake.py
@@ -5,15 +5,15 @@ from random import randrange
 import time
 import keyboard
 
-WIDTH = 602
-HEIGHT = 602
+WIDTH = 600
+HEIGHT = 600
 SIZE_BLOCK = 20
 #tkinter settings + canvas
 root = Tk()
 canvas = Canvas(root, width = WIDTH, height = HEIGHT, bg = '#2f353b')
 canvas.pack()
 lastPressed = 'a'
-snake = [[300,300], [320, 300], [340, 300]]
+snake = [[WIDTH//2,HEIGHT//2], [WIDTH//2 + SIZE_BLOCK,HEIGHT//2], [WIDTH//2 + 2*SIZE_BLOCK,HEIGHT//2]]
 #draw snake and apple
 def draw(snake, x, y):
     canvas.delete("all")
@@ -53,26 +53,33 @@ def endGame(snake):
         return True
     if((snake[0][1] < 0) | (snake[0][1] > 580)):
         return True
-
 #coord of apple
-x = randrange(0, 580, 20)
-y = randrange(0, 580, 20)
-draw(snake, x, y)
-root.update()
-while (keyboard.is_pressed('q') == False):
-    if(keyboard.is_pressed('w') & (lastPressed != 's')):
-        lastPressed = 'w'
-    elif(keyboard.is_pressed('a') & (lastPressed != 'd')):
-        lastPressed = 'a'
-    elif(keyboard.is_pressed('d') & (lastPressed != 'a')):
-        lastPressed = 'd'
-    elif(keyboard.is_pressed('s') & (lastPressed != 'w')):
-        lastPressed = 's'
-    move(snake, lastPressed, x, y)
-    if(snake[0]==[x,y]):
-        x = randrange(0, 580, 20)
-        y = randrange(0, 580, 20)
-    if(endGame(snake)):
-        break
-canvas.create_text(290, 290, text = "You lose", font="Arial 20", fill="white")
+def game():
+    lastPressed = 'a'
+    snake = [[HEIGHT//2,WIDTH//2], [HEIGHT//2 + SIZE_BLOCK,WIDTH//2], [HEIGHT//2 + 2*SIZE_BLOCK,WIDTH//2]]
+    x = randrange(0, 580, 20)
+    y = randrange(0, 580, 20)
+    draw(snake, x, y)
+    root.update()
+    while (keyboard.is_pressed('q') == False):
+        if(keyboard.is_pressed('w') & (lastPressed != 's')):
+            lastPressed = 'w'
+        elif(keyboard.is_pressed('a') & (lastPressed != 'd')):
+            lastPressed = 'a'
+        elif(keyboard.is_pressed('d') & (lastPressed != 'a')):
+            lastPressed = 'd'
+        elif(keyboard.is_pressed('s') & (lastPressed != 'w')):
+            lastPressed = 's'
+        move(snake, lastPressed, x, y)
+        if(snake[0]==[x,y]):
+            x = randrange(0, 580, 20)
+            y = randrange(0, 580, 20)
+        if(endGame(snake)):
+            break
+#Start game
+game()
+
+canvas.create_text(WIDTH//2,HEIGHT//2, text = "You lose", font="Arial 20", fill="white")
+canvas.create_text(WIDTH//2 - 10, HEIGHT//2 + 40, text = "Press F to start", font="Arial 15", fill="white")
+# need to start again
 root.mainloop()
